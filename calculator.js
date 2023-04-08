58f4894236668b0c5e13ed48d9388f9ed36489bf
Ivan Cheremisin <icheremisin@gmail.com>
2023-04-08 03:27:24 -0300

    Added all interactions Calculator is shit

--- calculator.js
+++ calculator.js
@@ -5,14 +5,30 @@ function calculate() {
     var var3 = document.getElementById("var3").value;
 
     // Convert var1 to a float to use in calculations
-    if (var1 == '1-10') {
+    if (var1 == "0") {
+      var1 = 0
+    } else if (var1 == '1-10') {
       var1 = 5;
     } else if (var1 == '11-20') {
       var1 = 15;
     } else {
       var1 = 25;
-    }
+    } 
 
+    if (var2 == 'Vehicle Maintenance') {
+      var2=1.4;
+    } else if (var2 == 'Driver Qualification') {
+      var2 = 1.1;
+    } else if (var2 == 'Unsafe Driving') {
+      var2 = 2;
+    } else if (var2 == 'Drug and Alcohol Testing') {
+      var2 = 2.4;
+    } else if (var2 == 'Hours of Service') {
+      var2 = 2.4;
+    } else if (var2 == 'Accidents') {
+      var2 = 2.4;
+    }
+ 
     // Convert var3 to a float by removing the percentage sign
     var3 = parseFloat(var3)/100;
 
